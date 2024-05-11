8b57acb7126d4908e8719e0ea8a7cc4476191de7
David A Roberts <d@vidr.cc>
2024-05-11 10:11:59 +1000

    Download tarballs for stage0

--- test/tarballs/download.sh
+++ test/tarballs/download.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 wget -c https://musl.libc.org/releases/musl-1.2.5.tar.gz
+wget -c https://musl.libc.org/releases/musl-1.1.24.tar.gz
 wget -c https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+wget -c https://ftp.gnu.org/gnu/make/make-3.82.tar.bz2
 wget -c https://ftp.gnu.org/gnu/bash/bash-5.2.21.tar.gz
 wget -c https://ftp.gnu.org/gnu/gawk/gawk-5.3.0.tar.xz
 wget -c https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz
+wget -c https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.bz2
 wget -c https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
-wget -c http://ftp.barfooze.de/pub/sabotage/tarballs/linux-headers-4.19.88-2.tar.xz
+wget -c http://ftp.barfooze.de/pub/sabotage/tarballs/linux-headers-4.19.88.tar.xz
+wget -c http://ftp.barfooze.de/pub/sabotage/tarballs/jobflow-1.3.1.tar.bz2
+wget -c http://ftp.gnu.org/gnu/gcc/gcc-3.4.6/gcc-core-3.4.6.tar.bz2
+wget -c http://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz
+wget -c https://busybox.net/downloads/busybox-1.33.0.tar.bz2
